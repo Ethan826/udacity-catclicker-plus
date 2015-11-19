f3fe470dcc69da836f29042e1d9a1fe36e57e221
Ethan Kent <ethankent@Ethans-MacBook-Pro.local>
2015-11-19 14:59:10 -0600

    Implemented fetchCatById() and several other details.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/// <reference path="./typings/jquery/jquery.d.ts"/>
 $(document).ready(function () {
     var Cat = (function () {
         function Cat(name, id, imgSrc) {
@@ -26,18 +27,45 @@ $(document).ready(function () {
     var cat1 = new Cat("Grumpy", "cat1", "http://i.dailymail.co.uk/i/pix/2014/08/05/1407225932091_wps_6_SANTA_MONICA_CA_AUGUST_04.jpg");
     var cat2 = new Cat("Happy", "cat2", "http://dc541.4shared.com/img/CNrXLjwN/s7/13cd1fd1a38/happy-cat.jpg");
     var cat3 = new Cat("Dopey", "cat3", "http://s-ak.buzzfed.com/static/enhanced/terminal01/2010/5/28/13/enhanced-buzz-7552-1275066424-6.jpg");
-    var cat4 = new Cat("Scary", "cat3", "http://i233.photobucket.com/albums/ee173/charbo187/cute-funny-animals-30.jpg");
-    var cat5 = new Cat("Stuck", "cat3", "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg");
+    var cat4 = new Cat("Scary", "cat4", "http://i233.photobucket.com/albums/ee173/charbo187/cute-funny-animals-30.jpg");
+    var cat5 = new Cat("Stuck", "cat5", "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg");
     var catArray = [cat1, cat2, cat3, cat4, cat5];
     var Page = (function () {
         function Page(cats) {
             this.cats = cats;
             this.buildDropDown();
+            this.activeCat = cats[Math.floor((Math.random() * cats.length))];
         }
         Page.prototype.buildDropDown = function () {
+            var catList = $("#cat-list");
+            var self = this;
             this.cats.forEach(function (cat) {
-                $("#cat-list").append("<option value=" + cat.getId() + ">" + cat.getName() + "</option>");
+                catList.append("<option value=" + cat.getId() + ">" + cat.getName() + "</option>");
             });
+            catList.change(function () {
+                self.activeCat = self.fetchCatById($(this).val());
+                console.log(self.activeCat);
+            });
+        };
+        Page.prototype.fetchCatById = function (id) {
+            console.log(id);
+            var counter = 0;
+            var answer = null;
+            while (counter < this.cats.length && !answer) {
+                var cat = this.cats[counter];
+                if (cat["id"] == id) {
+                    answer = cat;
+                }
+                counter += 1;
+            }
+            if (answer) {
+                return answer;
+            }
+            else {
+                throw "fetchCatByID() could not find the cat";
+            }
+        };
+        Page.prototype.renderActiveCat = function () {
         };
         return Page;
     })();
--- app.ts
+++ app.ts
@@ -38,31 +38,56 @@ $(document).ready(function(){
   var cat1 = new Cat("Grumpy", "cat1", "http://i.dailymail.co.uk/i/pix/2014/08/05/1407225932091_wps_6_SANTA_MONICA_CA_AUGUST_04.jpg");
   var cat2 = new Cat("Happy", "cat2", "http://dc541.4shared.com/img/CNrXLjwN/s7/13cd1fd1a38/happy-cat.jpg");
   var cat3 = new Cat("Dopey", "cat3", "http://s-ak.buzzfed.com/static/enhanced/terminal01/2010/5/28/13/enhanced-buzz-7552-1275066424-6.jpg");
-  var cat4 = new Cat("Scary", "cat3", "http://i233.photobucket.com/albums/ee173/charbo187/cute-funny-animals-30.jpg");
-  var cat5 = new Cat("Stuck", "cat3", "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg");
+  var cat4 = new Cat("Scary", "cat4", "http://i233.photobucket.com/albums/ee173/charbo187/cute-funny-animals-30.jpg");
+  var cat5 = new Cat("Stuck", "cat5", "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg");
   var catArray = [cat1, cat2, cat3, cat4, cat5];
 
   class Page {
 
     private cats: Cat[];
+    private activeCat: Cat;
 
-    buildDropDown(): void {
+    private buildDropDown(): void {
+      let catList = $("#cat-list");
+      var self = this;
       this.cats.forEach(function(cat){
-        $("#cat-list").append(`<option value=${cat.getId()}>${cat.getName()}</option>`)
+        catList.append(`<option value=${cat.getId()}>${cat.getName()}</option>`)
+      });
+      catList.change(function() {
+        self.activeCat = self.fetchCatById($(this).val());
+        console.log(self.activeCat);
       });
     }
 
     constructor(cats: Cat[]) {
       this.cats = cats;
       this.buildDropDown();
+      this.activeCat = cats[Math.floor((Math.random() * cats.length))];
+    }
+
+    private fetchCatById(id: string): Cat {
+      console.log(id);
+      let counter = 0;
+      let answer = null;
+      while(counter < this.cats.length && !answer) {
+        let cat = this.cats[counter];
+        if(cat["id"] == id) {
+          answer = cat;
+        }
+        counter += 1;
+      }
+      if(answer) {
+        return answer;
+      } else {
+        throw "fetchCatByID() could not find the cat";
+      }
+    }
+
+    private renderActiveCat(): void {
+/* Put the active cat on the page (call by constructor, too) */
     }
   }
 
-var page = new Page(catArray);
+  var page = new Page(catArray);
 
 });
-
-
-// names.forEach(function(n){
-//   console.log($("#cat-list").append(`<option>${n}</option>`));
-// });
